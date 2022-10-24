ba4a998fc8099c549a2b4913cb358b63bfae2e29
VanceXie <vancexie@gs.zzu.edu.cn>
2022-10-24 22:33:37 +0800

    test

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # 这是一个示例 Python 脚本。
+import cv2
+import sys
+import PyQt6
+
 
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
@@ -12,5 +16,7 @@ def print_hi(name):
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    p = cv2.imread("C:\\Users\\fy.xie\\Desktop\\20160225155336279.jpg", 0)
+    
+    cv2.imshow('image', p)
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pkg_resources
 from subprocess import call
+import sys
 
 for packages in [dist.project_name for dist in pkg_resources.working_set]:
     call("pip install --upgrade " + ''.join(packages), shell=True)
+
